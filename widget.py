d80297ed124862e760e6b473a3a7557bfd7ce3dd
Harjeevan Tanda <hstanda@gmail.com>
2025-08-17 00:55:22 +0530

    bug fix

--- widget.py
+++ widget.py
@@ -1,8 +1,8 @@
 import anywidget
 import traitlets
+import pathlib
 
 class CounterWidget(anywidget.AnyWidget):
-    _esm = "./_frontend/index.js"
-    _css = ""
+    _esm = pathlib.Path("_frontend/index.js")  # Anywidget will load this file
     count = traitlets.Int(0).tag(sync=True)
     dark = traitlets.Bool(False).tag(sync=True)
